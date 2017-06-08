9bc28d12198deaddd6b7fbd941615c555cf4edea
cwebsterz <cwebsterzornes@live.com>
2017-06-08 14:29:27 -0400

    started ls and initialized add

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-// const args = process.argv.splice(2)
-// const cmd = args[0]
-// const rest = args.slice(1)
-const [a, b, cmd, ...rest] = process.argv
-// commands
-const help = require('./cmds/help')
 
-function handler(cmd) {
+const [a, b, cmd, ...rest] = process.argv
+
+
+// commands
+const { get }  = require('./lib/store.js')
+const help = require('./cmds/help')
+const add = require('./cmds/add')
+const ls = require('./cmds/ls')
+
+
+function handler(cmd = ('')) {
   switch(cmd.toLowerCase()) {
     case 'help':
       return help()
+    case 'add':
+        return add()
+    case 'ls':
+        return ls(get())
     default:
       return 'Welcome to my todo app'
   }
