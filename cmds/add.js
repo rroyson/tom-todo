4baf3aaa3f0043392f29ef96bde3954b70ac4c7f
rroyson <frontiersons@gmail.com>
2017-06-08 16:14:38 -0400

    still on add

--- cmds/add.js
+++ cmds/add.js
@@ -1,14 +1,21 @@
 const { get, set } = require('../lib/store.js')
 const { append } = require ('ramda')
 const { map, reduce } = require ('../fp-utils')
+const ls = require ('./ls.js')
 const foo = [a, b, cmd, ...rest] = process.argv
 
 
 
 module.exports = function add (list) {
-  return appender(...rest, list)
+  const newTodoList = appender(...rest, list)
+  return set(newTodoList)
+
 }
 
 function appender (userInput, list) {
   return append(userInput, list)
 }
+
+
+
+//store.set(newTodoList)
