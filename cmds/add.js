7c62d84fb4e663a9604cff47ba0f13a16a27f5be
cwebsterz <cwebsterzornes@live.com>
2017-06-08 15:44:20 -0400

    made progress on add

--- cmds/add.js
+++ cmds/add.js
@@ -1,10 +1,12 @@
 const { get, set } = require('../lib/store.js')
 const { append } = require ('ramda')
-const { map } = require ('../fp-utils')
+const { map, reduce } = require ('../fp-utils')
+const foo = [a, b, cmd, ...rest] = process.argv
+
+
 
 module.exports = function add (list) {
-  return appender("cat",list)
-
+  return appender(...rest, list)
 }
 
 function appender (userInput, list) {
